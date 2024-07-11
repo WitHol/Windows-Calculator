a317d189e4ec008eabb6f888ea4e452488e9ef2f
WitHol <witekholdys@gmail.com>
2024-07-11 21:59:49 +0200

    Finished making UI

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ root = tk.Tk()
 root.title("Windows calculator")
 root.resizable(0,0)
 root.geometry('400x500')
+root.config(bg="#444444")
 
 # Making it so t hat when the window is closed, the program ends
 def closeWindow():
@@ -13,31 +14,55 @@ def closeWindow():
     exit(code=1)
 root.protocol("WM_DELETE_WINDOW", closeWindow)
 
-font = tkf.Font(family="Arial", size=14)
+# Fonts
+buttonFont = tkf.Font(family="Arial", size=50)
+textFont = tkf.Font(family="Arial", size=50)
+
+# An image, to make the buttons square
+image = tk.PhotoImage(width=6, height=1)
+
+
+#
+text = tk.Text(root, width=10, height=1, font=textFont, bg="#444444", fg='#cccccc', border = 0)
 
 # All the buttons
-buttonc = tk.Button(root, text="c", width=2, height=2, font=font, bg='#333333', fg='#dddddd')
-button1 = tk.Button(root, text="1", width=2, height=2, font=font, bg='#333333', fg='#dddddd')
-button2 = tk.Button(root, text="2", width=2, height=2, font=font, bg='#333333', fg='#dddddd')
-button3 = tk.Button(root, text="3", width=2, height=2, font=font, bg='#333333', fg='#dddddd')
-button4 = tk.Button(root, text="4", width=2, height=2, font=font, bg='#333333', fg='#dddddd')
-button5 = tk.Button(root, text="5", width=2, height=2, font=font, bg='#333333', fg='#dddddd')
-button6 = tk.Button(root, text="6", width=2, height=2, font=font, bg='#333333', fg='#dddddd')
-button7 = tk.Button(root, text="7", width=2, height=2, font=font, bg='#333333', fg='#dddddd')
-button8 = tk.Button(root, text="8", width=2, height=2, font=font, bg='#333333', fg='#dddddd')
-button9 = tk.Button(root, text="9", width=2, height=2, font=font, bg='#333333', fg='#dddddd')
-button0 = tk.Button(root, text="0", width=2, height=2, font=font, bg='#333333', fg='#dddddd')
+button_1 = tk.Button(root, image=image, compound='c', text="1", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_2 = tk.Button(root, image=image, compound='c', text="2", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_3 = tk.Button(root, image=image, compound='c', text="3", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_4 = tk.Button(root, image=image, compound='c', text="4", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_5 = tk.Button(root, image=image, compound='c', text="5", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_6 = tk.Button(root, image=image, compound='c', text="6", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_7 = tk.Button(root, image=image, compound='c', text="7", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_8 = tk.Button(root, image=image, compound='c', text="8", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_9 = tk.Button(root, image=image, compound='c', text="9", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_0 = tk.Button(root, image=image, compound='c', text="0", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_dot = tk.Button(root, image=image, compound='c', text=".", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_equal = tk.Button(root, image=image, compound='c', text="=", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_addition = tk.Button(root, image=image, compound='c', text="+", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_substraction = tk.Button(root, image=image, compound='c', text="-", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_multiplication = tk.Button(root, image=image, compound='c', text="×", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
+button_division = tk.Button(root, image=image, compound='c', text="÷", width=86, height=86, font=buttonFont, bg="#444444", fg='#dddddd', border=5)
 
-buttonc.grid(column=1, row=1, sticky='NSEW', rowspan=3)
-button1.grid(column=1, row=2, sticky='NSEW')
-button2.grid(column=2, row=2, sticky='NSEW')
-button3.grid(column=3, row=2, sticky='NSEW')
-button4.grid(column=1, row=3, sticky='NSEW')
-button5.grid(column=2, row=3, sticky='NSEW')
-button6.grid(column=3, row=3, sticky='NSEW')
-button7.grid(column=1, row=4, sticky='NSEW')
-button8.grid(column=2, row=4, sticky='NSEW')
-button9.grid(column=3, row=4, sticky='NSEW')
-button0.grid(column=2, row=5, sticky='NSEW')
+text.grid(column=1, row=1, columnspan=4, pady=11, sticky="e")
+
+button_1.grid(column=1, row=2)
+button_2.grid(column=2, row=2)
+button_3.grid(column=3, row=2)
+button_division.grid(column=4, row=2)
+
+button_4.grid(column=1, row=3)
+button_5.grid(column=2, row=3)
+button_6.grid(column=3, row=3)
+button_multiplication.grid(column=4, row = 3)
+
+button_7.grid(column=1, row=4)
+button_8.grid(column=2, row=4)
+button_9.grid(column=3, row=4)
+button_substraction.grid(column=4, row=4)
+
+button_dot.grid(column=1, row=5)
+button_0.grid(column=2, row=5)
+button_equal.grid(column=3, row=5)
+button_addition.grid(column=4, row=5)
 
 root.mainloop()
